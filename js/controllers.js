acbba11efde7bfc14396dad505363a0fbcc1fe9d
kmiller <kyle.miller@ctl.io>
2016-04-20 15:36:44 -0500

    Search now works by name

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,8 @@
 var angularApp = angular.module("bdoApp", []);
  angularApp.controller("SearchController", function($scope) {
 	$scope.pointsOfInterest = new Array();
+	
+	$scope.rand = function(r) {
+		return r + Math.random;
+	}
  });
